508fcbf1988ef3e17e83b1445e98f17b7cb93862
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2015-05-15 23:13:51 +0200

    Implement empty version.

---
+++ src/main/java/net/oneandone/maven/releasepolicies/ScmVersionPolicy.java
@@ -0,0 +1,28 @@
+package net.oneandone.maven.releasepolicies;
+
+import org.apache.maven.shared.release.policy.PolicyException;
+import org.apache.maven.shared.release.policy.version.VersionPolicy;
+import org.apache.maven.shared.release.policy.version.VersionPolicyRequest;
+import org.apache.maven.shared.release.policy.version.VersionPolicyResult;
+import org.apache.maven.shared.release.versions.VersionParseException;
+import org.codehaus.plexus.component.annotations.Component;
+
+/**
+ * A {@link VersionPolicy} implementation that retrieve the latest version from the SCM and just increases.
+ */
+@Component(
+        role = VersionPolicy.class,
+        hint = "ScmVersionPolicy",
+        description = "A VersionPolicy implementation that Retrieve the latest version from the SCM and just increases."
+)
+public class ScmVersionPolicy implements VersionPolicy {
+    @Override
+    public VersionPolicyResult getReleaseVersion(VersionPolicyRequest request) throws PolicyException, VersionParseException {
+        return null;
+    }
+
+    @Override
+    public VersionPolicyResult getDevelopmentVersion(VersionPolicyRequest request) throws PolicyException, VersionParseException {
+        return null;
+    }
+}
