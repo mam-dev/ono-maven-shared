89d88e25f1f6b47528c3864ab756477bf1135764
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2015-05-16 22:50:40 +0200

    Retrieve currentVersion from Artifactory.

--- src/main/java/net/oneandone/maven/releasepolicies/ArtifactoryVersionPolicy.java
+++ src/main/java/net/oneandone/maven/releasepolicies/ArtifactoryVersionPolicy.java
@@ -4,8 +4,15 @@ import org.apache.maven.shared.release.policy.PolicyException;
 import org.apache.maven.shared.release.policy.version.VersionPolicy;
 import org.apache.maven.shared.release.policy.version.VersionPolicyRequest;
 import org.apache.maven.shared.release.policy.version.VersionPolicyResult;
+import org.apache.maven.shared.release.versions.DefaultVersionInfo;
 import org.apache.maven.shared.release.versions.VersionParseException;
+import org.apache.maven.shared.utils.io.IOUtil;
 import org.codehaus.plexus.component.annotations.Component;
+
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
 
 /**
  * A {@link VersionPolicy} implementation that retrieve the latest version from the SCM and just increases.
@@ -17,17 +24,35 @@ import org.codehaus.plexus.component.annotations.Component;
 )
 public class ArtifactoryVersionPolicy implements VersionPolicy {
 
+    private static final String HTTP_ARTIFACTORY = "http://repo.jfrog.org/artifactory";
+    private static final String REPOSITORIES = "repo1-cache";
+
     @Override
     public VersionPolicyResult getReleaseVersion(VersionPolicyRequest request) throws PolicyException, VersionParseException {
         final VersionPolicyResult versionPolicyResult = new VersionPolicyResult();
-        versionPolicyResult.setVersion("1.5.7");
+        final String groupId = request.getMetaData().getGroupId();
+        final String artifactId = request.getMetaData().getArtifactId();
+        final String currentVersion;
+        try {
+            final URL url = new URL(String.format(
+                    Locale.ENGLISH,
+                    HTTP_ARTIFACTORY + "/api/search/latestVersion?g=%s&a=%s&repos=%s",
+                    groupId, artifactId, REPOSITORIES));
+            try(final InputStream stream = url.openStream()) {
+                currentVersion = IOUtil.toString(stream, "UTF-8");
+            }
+        } catch (java.io.IOException e) {
+            throw new RuntimeException("Unable to access " + HTTP_ARTIFACTORY, e);
+        }
+        final DefaultVersionInfo versionInfo = new DefaultVersionInfo(currentVersion);
+        versionPolicyResult.setVersion(versionInfo.getNextVersion().getReleaseVersionString());
         return versionPolicyResult;
     }
 
     @Override
     public VersionPolicyResult getDevelopmentVersion(VersionPolicyRequest request) throws PolicyException, VersionParseException {
         final VersionPolicyResult versionPolicyResult = new VersionPolicyResult();
-        versionPolicyResult.setVersion("1-SNAPSHOT");
+        versionPolicyResult.setVersion(request.getMetaData().getVersion());
         return versionPolicyResult;
     }
 }
--- src/test/java/net/oneandone/maven/releasepolicies/ArtifactoryVersionPolicyTest.java
+++ src/test/java/net/oneandone/maven/releasepolicies/ArtifactoryVersionPolicyTest.java
@@ -1,5 +1,7 @@
 package net.oneandone.maven.releasepolicies;
 
+import org.apache.maven.artifact.repository.metadata.Metadata;
+import org.apache.maven.shared.release.policy.version.VersionPolicyRequest;
 import org.apache.maven.shared.release.policy.version.VersionPolicyResult;
 import org.junit.Test;
 
@@ -11,14 +13,27 @@ public class ArtifactoryVersionPolicyTest {
     @Test
     public void testGetReleaseVersion() throws Exception {
         final ArtifactoryVersionPolicy subjectUnderTest = new ArtifactoryVersionPolicy();
-        final VersionPolicyResult releaseVersion = subjectUnderTest.getReleaseVersion(null);
+        final VersionPolicyRequest versionPolicyRequest = createVersionPolicyRequest();
+        final VersionPolicyResult releaseVersion = subjectUnderTest.getReleaseVersion(versionPolicyRequest);
         assertThat(releaseVersion.getVersion()).isEqualTo("1.5.7");
     }
 
     @Test
     public void testGetDevelopmentVersion() throws Exception {
         final ArtifactoryVersionPolicy subjectUnderTest = new ArtifactoryVersionPolicy();
-        final VersionPolicyResult releaseVersion = subjectUnderTest.getDevelopmentVersion(null);
+        final VersionPolicyRequest versionPolicyRequest = createVersionPolicyRequest();
+        final VersionPolicyResult releaseVersion = subjectUnderTest.getDevelopmentVersion(versionPolicyRequest);
         assertThat(releaseVersion.getVersion()).isEqualTo("1-SNAPSHOT");
+    }
+
+    private VersionPolicyRequest createVersionPolicyRequest() {
+        final VersionPolicyRequest versionPolicyRequest = new VersionPolicyRequest();
+        versionPolicyRequest.setVersion("1-SNAPSHOT");
+        final Metadata metaData = new Metadata();
+        metaData.setGroupId("net.oneandone.maven.poms");
+        metaData.setArtifactId("foss-parent");
+        metaData.setVersion("1-SNAPSHOT");
+        versionPolicyRequest.setMetaData(metaData);
+        return versionPolicyRequest;
     }
 }
