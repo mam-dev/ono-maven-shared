4e528b7023f907b5dfa3145d737c1377789ec488
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2015-07-25 22:09:06 +0200

    First test for developmentVersion

---
+++ src/main/java/net/oneandone/maven/shared/versionpolicies/ChangesVersionPolicy.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 1&1 Internet AG, https://github.com/1and1/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.oneandone.maven.shared.versionpolicies;
+
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.release.policy.PolicyException;
+import org.apache.maven.shared.release.policy.version.VersionPolicy;
+import org.apache.maven.shared.release.policy.version.VersionPolicyRequest;
+import org.apache.maven.shared.release.policy.version.VersionPolicyResult;
+import org.apache.maven.shared.release.versions.VersionParseException;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+
+/**
+ * A {@link VersionPolicy} implementation that retrieves the latest version from the changes file..
+ */
+@Component(
+        role = VersionPolicy.class,
+        hint = "ONOChangesVersionPolicy",
+        description = "A VersionPolicy implementation that retrieves the latest version from the changes file."
+)
+public class ChangesVersionPolicy implements VersionPolicy {
+
+    @Requirement
+    MavenProject mavenProject;
+
+    ChangesVersionPolicy(MavenProject mavenProject) {
+        this.mavenProject = mavenProject;
+    }
+
+    @Override
+    public VersionPolicyResult getReleaseVersion(VersionPolicyRequest request) throws PolicyException, VersionParseException {
+
+        return null;
+    }
+
+    @Override
+    public VersionPolicyResult getDevelopmentVersion(VersionPolicyRequest request) throws PolicyException, VersionParseException {
+        final VersionPolicyResult versionPolicyResult = new VersionPolicyResult();
+        versionPolicyResult.setVersion(mavenProject.getVersion());
+        return versionPolicyResult;
+    }
+}
---
+++ src/test/groovy/net/oneandone/maven/shared/versionpolicies/ChangesVersionPolicyTest.groovy
@@ -0,0 +1,43 @@
+/**
+ * Copyright 1&1 Internet AG, https://github.com/1and1/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.oneandone.maven.shared.versionpolicies
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+class ChangesVersionPolicyTest extends Specification implements AbstractVersionPolicyTrait {
+
+
+    def testGetReleaseVersion() {
+        given:
+        def mavenProject = createMavenProject();
+        @Subject
+        def subjectUnderTest = new ChangesVersionPolicy(mavenProject);
+
+        expect:
+        subjectUnderTest.getReleaseVersion(VPR_DOES_NOT_MATTER) is null
+    }
+
+    def testGetDevelopmentVersion() throws Exception {
+        given:
+        def mavenProject = createMavenProject();
+        @Subject
+        def subjectUnderTest = new ChangesVersionPolicy(mavenProject);
+
+        expect:
+        subjectUnderTest.getDevelopmentVersion(VPR_DOES_NOT_MATTER).version == mavenProject.getVersion()
+    }
+}
