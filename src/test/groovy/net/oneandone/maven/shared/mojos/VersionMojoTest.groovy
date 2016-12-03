62a05f0da72de4d93593fbe8995eaed664e63827
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2016-12-03 21:39:34 +0100

    Add more tests.

--- src/test/groovy/net/oneandone/maven/shared/mojos/VersionMojoTest.groovy
+++ src/test/groovy/net/oneandone/maven/shared/mojos/VersionMojoTest.groovy
@@ -1,6 +1,8 @@
 package net.oneandone.maven.shared.mojos
 
 import net.oneandone.maven.shared.versionpolicies.AbstractVersionPolicyTrait
+import net.oneandone.maven.shared.versionpolicies.BuildNumberVersionPolicy
+import net.oneandone.maven.shared.versionpolicies.ChangesVersionPolicy
 import org.apache.maven.execution.MavenSession
 import org.apache.maven.plugin.MojoExecutionException
 import org.apache.maven.plugin.logging.Log
@@ -11,16 +13,14 @@ import spock.lang.Specification
 import spock.lang.Subject
 
 class VersionMojoTest extends Specification implements AbstractVersionPolicyTrait {
-    def "Execute with given changes and two releases"() {
+    def "Execute with default policyVersionId"() {
         given:
         def session = Mock(MavenSession)
         def properties = new Properties()
-        def versionPolicies = new HashMap<String, VersionPolicy>()
-        versionPolicies['default'] = new DefaultVersionPolicy()
         session.getUserProperties() >> properties
         def project = new MavenProject(artifactId: "foo", version: "3-SNAPSHOT", executionRoot: true)
         @Subject
-        def sut = new VersionMojo(project, session, versionPolicies, 'default')
+        def sut = new VersionMojo(project, session, createVersionPolicies(project), 'default')
         sut.log = createQuietLogger()
 
         when:
@@ -31,6 +31,26 @@ class VersionMojoTest extends Specification implements AbstractVersionPolicyTrai
         properties['developmentVersion'] == '4-SNAPSHOT'
         properties['ONOCurrentVersion'] == 'NOT_SUPPORTED'
         properties['tag'] == 'foo-3'
+    }
+
+    def "Execute with ChangesVersionPolicy"() {
+        given:
+        def session = Mock(MavenSession)
+        def properties = new Properties()
+        session.getUserProperties() >> properties
+        def project = new MavenProject(artifactId: "foo", version: "3-SNAPSHOT", executionRoot: true)
+        @Subject
+        def sut = new VersionMojo(project, session, createVersionPolicies(project), 'ONOChangesVersionPolicy')
+        sut.log = createQuietLogger()
+
+        when:
+        sut.execute()
+
+        then:
+        properties['releaseVersion'] == '3.0.2'
+        properties['developmentVersion'] == '3-SNAPSHOT'
+        properties['ONOCurrentVersion'] == '3.0.1'
+        properties['tag'] == 'foo-3.0.2'
     }
 
     def 'Do nothing for none executionRoot'() {
@@ -56,11 +76,9 @@ class VersionMojoTest extends Specification implements AbstractVersionPolicyTrai
         def properties = new Properties()
         session.getUserProperties() >> properties
         def project = new MavenProject(artifactId: "foo", version: "3-SNAPSHOT", executionRoot: true)
-        def versionPolicies = new HashMap<String, VersionPolicy>()
-        versionPolicies['default'] = new DefaultVersionPolicy()
         session.getUserProperties() >> properties
         @Subject
-        def sut = new VersionMojo(project, session, versionPolicies, 'unknown')
+        def sut = new VersionMojo(project, session, createVersionPolicies(project), 'unknown')
         sut.log = createQuietLogger()
 
         when:
@@ -77,6 +95,14 @@ class VersionMojoTest extends Specification implements AbstractVersionPolicyTrai
      */
     Log createQuietLogger() {
         return Mock(Log)
+    }
+
+    Map<String, VersionPolicy> createVersionPolicies(MavenProject project) {
+        return [
+            'default': new DefaultVersionPolicy(),
+            'ONOBuildNumberVersionPolicy': new BuildNumberVersionPolicy(project, ['BUILD_NUMBER': '1']),
+            'ONOChangesVersionPolicy': new ChangesVersionPolicy(project, "target/test-classes/changes/twoversions.xml")
+        ]
     }
 
 }
